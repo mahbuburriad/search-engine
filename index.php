606f34c26f91d6e5b2dacba2a6b0184558211223
mahbuburriad <mahbubur.riad@northsouth.edu>
2018-11-19 00:19:46 +0600

    logo and index page complete css and html

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="assets/images/logo.png" rel=icon type="image/png">
     <title>Search Engine</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+ <div class="wrapper indexPage">
+  <div class="mainSection">
+      <div class="logoContainer">
+         <img src="assets/images/logo.png" alt="riddle search image">
+      </div>
+      <div class="searchContainer">
+         <form action="search.php" method="get">
+          <input type="text" class="searchBox" name="term">
+          <input type="submit" class="searchButton" value="Search">
+          </form>
+      </div>
+  </div>
+  </div>
 </body>
 </html>
