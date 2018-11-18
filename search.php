ef7c7f6a8959a49f4648e931f9d339baeba441a2
mahbuburriad <mahbubur.riad@northsouth.edu>
2018-11-19 01:16:29 +0600

    space deduct and beautify

--- search.php
+++ search.php
@@ -5,11 +5,7 @@ $term = $_GET["term"];
 else{
     exit( "You Must Enter Some text Search");
 }
-
 $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
-
-
-
 ?>
 
 
@@ -28,9 +24,9 @@ $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
         <div class="header">
             <div class="headerContent">
                 <div class="logoContainer">
-                   <a href="index.php"><img src="assets/images/logo-name.png" alt="riddle search image"></a>
+                    <a href="index.php"><img src="assets/images/logo-name.png" alt="riddle search image"></a>
                 </div>
-                
+
                 <div class="searchContainer">
                     <form action="search.php" method="get">
                         <div class="searchBarContainer">
@@ -41,12 +37,13 @@ $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
                 </div>
             </div>
             <div class="tabsContainer">
-                    <ul class="tabList">
-                        <li class="<?php echo $type == 'sites' ? 'active' : '' ?>"><a href='<?php echo "search.php?term=$term&type=sites"; ?>'>Sites</a></li>
-                         <li class="<?php echo $type == 'images' ? 'active' : '' ?>"><a href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a></li>
-                    </ul>
-                </div>
+                <ul class="tabList">
+                    <li class="<?php echo $type == 'sites' ? 'active' : '' ?>"><a href='<?php echo "search.php?term=$term&type=sites"; ?>'>Sites</a></li>
+                    <li class="<?php echo $type == 'images' ? 'active' : '' ?>"><a href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
