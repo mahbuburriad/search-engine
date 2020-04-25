a18a8cde8163f0239ebb1a6a686b7075e6775b02
Mahbubur Riad <mahbubur.riad@gmail.com>
2020-04-25 21:30:52 +0600

    code modified to web crawler

--- search.php
+++ search.php
@@ -1,9 +1,10 @@
 <?php
-if(isset($_GET["term"])){
-$term = $_GET["term"];
-}
-else{
-    exit( "You Must Enter Some text Search");
+
+include("config.php");
+if (isset($_GET["term"])) {
+    $term = $_GET["term"];
+} else {
+    exit("You Must Enter Some text Search");
 }
 $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
 ?>
@@ -20,30 +21,32 @@ $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
 </head>
 
 <body>
-    <div class="wrapper">
-        <div class="header">
-            <div class="headerContent">
-                <div class="logoContainer">
-                    <a href="index.php"><img src="assets/images/logo-name.png" alt="riddle search image"></a>
-                </div>
-
-                <div class="searchContainer">
-                    <form action="search.php" method="get">
-                        <div class="searchBarContainer">
-                            <input type="text" class="searchBox" name="term">
-                            <button class="searchButton"><img src="assets/images/search.png" alt="Search Icon"></button>
-                        </div>
-                    </form>
-                </div>
+<div class="wrapper">
+    <div class="header">
+        <div class="headerContent">
+            <div class="logoContainer">
+                <a href="index.php"><img src="assets/images/logo-name.png" alt="riddle search image"></a>
             </div>
-            <div class="tabsContainer">
-                <ul class="tabList">
-                    <li class="<?php echo $type == 'sites' ? 'active' : '' ?>"><a href='<?php echo "search.php?term=$term&type=sites"; ?>'>Sites</a></li>
-                    <li class="<?php echo $type == 'images' ? 'active' : '' ?>"><a href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a></li>
-                </ul>
+
+            <div class="searchContainer">
+                <form action="search.php" method="get">
+                    <div class="searchBarContainer">
+                        <input type="text" class="searchBox" name="term">
+                        <button class="searchButton"><img src="assets/images/search.png" alt="Search Icon"></button>
+                    </div>
+                </form>
             </div>
         </div>
+        <div class="tabsContainer">
+            <ul class="tabList">
+                <li class="<?php echo $type == 'sites' ? 'active' : '' ?>"><a
+                            href='<?php echo "search.php?term=$term&type=sites"; ?>'>Sites</a></li>
+                <li class="<?php echo $type == 'images' ? 'active' : '' ?>"><a
+                            href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a></li>
+            </ul>
+        </div>
     </div>
+</div>
 </body>
 
 </html>
