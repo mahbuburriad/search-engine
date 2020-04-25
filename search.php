4760408579de82f25246f2792d248bedbdcaf748
Mahbubur Riad <mahbubur.riad@gmail.com>
2020-04-25 22:00:44 +0600

    outputing site results as title

--- search.php
+++ search.php
@@ -53,6 +53,8 @@ $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
         $resultsProvider = new SiteResultsProvider($con);
         $numResults =  $resultsProvider->getNumResults($term);
         echo "<p class='resultsCount'>$numResults results found</p>";
+
+        echo $resultsProvider->getResultsHtml(1,20,$term);
         ?>
 
     </div>
